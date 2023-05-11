037281600d982159258d86e280cf2080c12c0491
Md Sayed Hossen <127623616+shbablu23@users.noreply.github.com>
2023-05-11 13:12:58 +0600

    Add files via upload

---
+++ inc/env.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = 'localhost';
+$db_user_name = 'root';
+$db_password = '';
+$db_name = 'yammy_foods';
+
+//mysqli_connect($db_host, $db_user_name, $db_password, $db_name);
+$dbconnect= mysqli_connect($db_host, $db_user_name, $db_password, $db_name);
---
+++ inc/login_header.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login / Register</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/animate.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/login/css/util.css">
+<link rel="stylesheet" type="text/css" href="./assets/login/css/main.css">
+
+<meta name="robots" content="noindex, follow">
+<script nonce="728baae0-4ba9-4982-904c-f331bf389295">(function(w,d){!function(bv,bw,bx,by){bv[bx]=bv[bx]||{};bv[bx].executed=[];bv.zaraz={deferred:[],listeners:[]};bv.zaraz.q=[];bv.zaraz._f=function(bz){return function(){var bA=Array.prototype.slice.call(arguments);bv.zaraz.q.push({m:bz,a:bA})}};for(const bB of["track","set","debug"])bv.zaraz[bB]=bv.zaraz._f(bB);bv.zaraz.init=()=>{var bC=bw.getElementsByTagName(by)[0],bD=bw.createElement(by),bE=bw.getElementsByTagName("title")[0];bE&&(bv[bx].t=bw.getElementsByTagName("title")[0].text);bv[bx].x=Math.random();bv[bx].w=bv.screen.width;bv[bx].h=bv.screen.height;bv[bx].j=bv.innerHeight;bv[bx].e=bv.innerWidth;bv[bx].l=bv.location.href;bv[bx].r=bw.referrer;bv[bx].k=bv.screen.colorDepth;bv[bx].n=bw.characterSet;bv[bx].o=(new Date).getTimezoneOffset();if(bv.dataLayer)for(const bI of Object.entries(Object.entries(dataLayer).reduce(((bJ,bK)=>({...bJ[1],...bK[1]})))))zaraz.set(bI[0],bI[1],{scope:"page"});bv[bx].q=[];for(;bv.zaraz.q.length;){const bL=bv.zaraz.q.shift();bv[bx].q.push(bL)}bD.defer=!0;for(const bM of[localStorage,sessionStorage])Object.keys(bM||{}).filter((bO=>bO.startsWith("_zaraz_"))).forEach((bN=>{try{bv[bx]["z_"+bN.slice(7)]=JSON.parse(bM.getItem(bN))}catch{bv[bx]["z_"+bN.slice(7)]=bM.getItem(bN)}}));bD.referrerPolicy="origin";bD.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(bv[bx])));bC.parentNode.insertBefore(bD,bC)};["complete","interactive"].includes(bw.readyState)?zaraz.init():bv.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+<body>
+<div class="limiter">
+<div class="container-login100">
+<div class="wrap-login100">
+<div class="login100-pic js-tilt" data-tilt>
+<img src="./assets/login/img/img-01.webp" alt="IMG">
+</div>
